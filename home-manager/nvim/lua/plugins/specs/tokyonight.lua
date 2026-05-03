742c4513c664f7344fe958aca297df0aa0a4f47d
beso1225 <yutaroformisc@gmail.com>
2026-05-03 18:23:39 +0900

    feat: make nvim transparent

--- home-manager/nvim/lua/plugins/specs/tokyonight.lua
+++ home-manager/nvim/lua/plugins/specs/tokyonight.lua
@@ -1,9 +1,18 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("tokyonight-night")
-        end,
-    },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          comments = { italic = false },
+          sidebars = "transparent",
+          floats = "transparent",
+        }
+      })
+      vim.cmd [[colorscheme tokyonight-night]]
+    end,
+  },
 }
