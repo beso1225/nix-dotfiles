5449c4cbd07a4f47c4496af63e53bd1bdc791cc9
beso1225 <yutaroformisc@gmail.com>
2026-05-11 20:50:55 +0900

    feat: add basic settings

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -4,9 +4,26 @@
 }:
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
-  system.stateVersion = 6;
+  system = {
+    # Please read the documentation of stateVersion before changing this.
+    stateVersion = 6;
+
+    # setting to record of commit hash of the configuration
+    configurationRevision = self.rev or self.dirtyRev or null;
+
+    primaryUser = "yutarotakagi";
+  };
+
+  # Set the home directory 
+  users.users.yutarotakagi.home = "/Users/yutarotakagi";
+
+  # Disable the management of the Nix by nix-darwin, since Home Manager will manage it.
   nix.enable = false;
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Set the default shell to zsh
+  programs.zsh.enable = true;
+  
+  # Enable Touch ID authentication for sudo and reattach to the session after authentication.
   security.pam.services.sudo_local = {
     touchIdAuth = true;
     reattach = true;
